e15442ee079e80ca9cdbae0cd97b35f6ed7b4f1a
Younggyu Kim <dhflqhd687@gmail.com>
2022-02-12 19:22:43 -0600

    merging again

--- bikeshare.py
+++ bikeshare.py
@@ -108,7 +108,7 @@ def load_data(city, month, day):
 
         # filter by day of week if applicable
     if day != 'all':
-        
+
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
 
